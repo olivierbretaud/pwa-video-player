d7797d54439e584c8830b0d39558b9c88c3feb1a
Olivier Bretaud <olivierbretaud@hotmail.fr>
2019-06-24 15:57:35 +0200

    Updates

---
+++ precache-manifest.75c52687884445ba0f0b66a90f401876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f3fbe04a47274ea02360863ce7b1c0",
+    "url": "/pwa-video-player/index.html"
+  },
+  {
+    "revision": "0572212090d6f1636362",
+    "url": "/pwa-video-player/static/css/main.9006df9e.chunk.css"
+  },
+  {
+    "revision": "163476f43d60e4323c11",
+    "url": "/pwa-video-player/static/js/2.e500f8de.chunk.js"
+  },
+  {
+    "revision": "0572212090d6f1636362",
+    "url": "/pwa-video-player/static/js/main.d6bc4658.chunk.js"
+  },
+  {
+    "revision": "59aaa8c86a90f580d9d4",
+    "url": "/pwa-video-player/static/js/runtime~main.ca2dd1db.js"
+  }
+]);
