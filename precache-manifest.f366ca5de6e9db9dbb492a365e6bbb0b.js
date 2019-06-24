a5041a9dbfff182bccb6dd7644796f13dea0f84e
Olivier Bretaud <olivierbretaud@hotmail.fr>
2019-06-24 17:48:26 +0200

    Updates

--- precache-manifest.f366ca5de6e9db9dbb492a365e6bbb0b.js
+++ precache-manifest.f366ca5de6e9db9dbb492a365e6bbb0b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1d1e91bf66bdc7cee062920a133ac973",
+    "revision": "74e21045943e896e171abd3f4edba318",
     "url": "/pwa-video-player/index.html"
   },
   {
-    "revision": "a6ce39ee2c41cd689483",
+    "revision": "3b6539098db96463581f",
     "url": "/pwa-video-player/static/css/main.779199f4.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pwa-video-player/static/js/2.3310f33a.chunk.js"
   },
   {
-    "revision": "a6ce39ee2c41cd689483",
-    "url": "/pwa-video-player/static/js/main.ac4fbc76.chunk.js"
+    "revision": "3b6539098db96463581f",
+    "url": "/pwa-video-player/static/js/main.b917a3f4.chunk.js"
   },
   {
     "revision": "59aaa8c86a90f580d9d4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pwa-video-player/precache-manifest.f2ad67725895a0a17b770e5f70758bb7.js"
+  "/pwa-video-player/precache-manifest.f366ca5de6e9db9dbb492a365e6bbb0b.js"
 );
 
 self.addEventListener('message', (event) => {
